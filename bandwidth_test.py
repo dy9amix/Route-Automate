c94e35541a5b252d34978d602ea18254c2c89266
orodeji oluwasegun <32060683+dy9amix@users.noreply.github.com>
2021-10-21 12:21:29 +0100

    Update bandwidth_test.py

--- bandwidth_test.py
+++ bandwidth_test.py
@@ -51,12 +51,9 @@ def db_access():
     secret_file.close()
     # Fetch the service account key JSON file contents
     cred_path = os.getcwd() + '/secrect.json'
-    with open(cred_path) as f:
-      contents = f.read()
-      print(contents)
-    cred = credentials.Certificate(cred_path)
+    #cred = credentials.Certificate(cred_path)
     # Initialize the app with a service account, granting admin privileges
-    firebase_admin.initialize_app(cred, {
+    firebase_admin.initialize_app( {
         'databaseURL': f'{os.environ["firebase_url"]}'
     })
     # As an admin, the app has access to read and write all data, regradless of Security Rules
